fbcaf6dd206f03dc017d6ae808fb6128a847419e
Your Name <githakawendowanjiru@gmail.com>
2022-08-08 01:22:13 -0700

    task1

--- _printf.c
+++ _printf.c
@@ -1,8 +1,12 @@
 #include "main.h"
+
 /**
-* _printf - printf function
-* @format: const char pointer
-* Return: b_len
+* _printf - produces output according to a format
+* @format: format string containing the characters and the specifiers
+* Description: this function will call the get_print() function that will
+* determine which printing function to call depending on the conversion
+* specifiers contained into fmt
+* Return: length of the formatted output string
 */
 int _printf(const char *format, ...)
 {
@@ -34,7 +38,8 @@ pfunc = get_print(*p);
 count += (pfunc)
 ? pfunc(arguments, &flags)
 : _printf("%%%c", *p);
-} else
+}
+else
 count += _putchar(*p);
 }
 _putchar(-1);
